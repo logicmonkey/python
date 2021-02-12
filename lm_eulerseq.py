332fa6d8d36e8c1065b0ddb5bab46f7ee9f121e4
Piers Barber <piers.barber@logicmonkey.co.uk>
2021-02-12 00:34:58 +0000

    euler's equation won't run

--- lm_eulerseq.py
+++ lm_eulerseq.py
@@ -18,15 +18,7 @@ import tkinter
 import time
 import numpy as np
 
-black = "#2e2e2e"
-grey = "#797979"
-white = "#d6d6d6"
-yellow = "#e5b567"
-green = "#b4d273"
-orange = "#e87d3e"
-purple = "#9e86c8"
-pink = "#b05279"
-blue = "#6c99bb"
+with open('lm_bolokai.py') as f: exec(f.read())
 
 R = 2 # dot radius
 DELTA = 1
@@ -47,30 +39,35 @@ def create_window():
 # Create canvas and add to main window
 def create_canvas(window):
   canvas = tkinter.Canvas(window)
-  canvas.configure(bg=black)
+  canvas.configure(bg=darkgray)
   canvas.pack(fill="both", expand=True)
   return canvas
+
+def calcs(t):
+  sin = np.sin(2*np.pi*FREQ*t/(2*XMAX/3)) * SCALE
+  xpos_sin = XMAX/3 + t
+  ypos_sin = 5*YMAX/6 - YMAX/6 * sin
+
+  cos = np.cos(2*np.pi*FREQ*t/(2*XMAX/3)) * SCALE
+  xpos_cos = XMAX/6 + XMAX/6*cos
+  ypos_cos = 2*YMAX/3 - t
+
+  xpos_circ = XMAX/6 + XMAX/6*cos
+  ypos_circ = 5*YMAX/6 - YMAX/6*sin
+  return xpos_sin, ypos_sin, xpos_cos, ypos_cos, xpos_circ, ypos_circ
 
 # Animation loop
 def animate(window, canvas):
   # set initial coords
   xt = 0
   R=1
-  while xt < 2*XMAX/3:
-    sin = np.sin(2*np.pi*FREQ*xt/(2*XMAX/3)) * SCALE
-    xpos_sin = XMAX/3 + xt
-    ypos_sin = 5*YMAX/6 - YMAX/6 * sin
+  while xt < 2*XMAX/3: # draw the background images once only
 
-    cos = np.cos(2*np.pi*FREQ*xt/(2*XMAX/3)) * SCALE
-    xpos_cos = XMAX/6 + XMAX/6*cos
-    ypos_cos = 2*YMAX/3 - xt
+    xpos_sin, ypos_sin, xpos_cos, ypos_cos, xpos_circ, ypos_circ = calcs(xt)
 
-    xpos_circ = XMAX/6 + XMAX/6*cos
-    ypos_circ = 5*YMAX/6 - YMAX/6*sin
-
-    canvas.create_oval(xpos_sin-R, ypos_sin-R, xpos_sin+R, ypos_sin+R, outline=purple, fill=purple)
-    canvas.create_oval(xpos_cos-R, ypos_cos-R, xpos_cos+R, ypos_cos+R, outline=pink, fill=pink)
-    canvas.create_oval(xpos_circ-R, ypos_circ-R, xpos_circ+R, ypos_circ+R, outline=green, fill=green)
+    canvas.create_oval(xpos_sin-R, ypos_sin-R, xpos_sin+R, ypos_sin+R, outline=darkcyan, fill=darkcyan)
+    canvas.create_oval(xpos_cos-R, ypos_cos-R, xpos_cos+R, ypos_cos+R, outline=pastelorange, fill=pastelorange)
+    canvas.create_oval(xpos_circ-R, ypos_circ-R, xpos_circ+R, ypos_circ+R, outline=darkgreen, fill=darkgreen)
 
     xt += 1/FREQ
 
@@ -78,24 +75,15 @@ def animate(window, canvas):
 
   while True:
     # Draw everything
-    sin = np.sin(2*np.pi*FREQ*xt/(2*XMAX/3)) * SCALE
-    xpos_sin = XMAX/3 + xt
-    ypos_sin = 5*YMAX/6 - YMAX/6 * sin
+    xpos_sin, ypos_sin, xpos_cos, ypos_cos, xpos_circ, ypos_circ = calcs(xt)
 
-    cos = np.cos(2*np.pi*FREQ*xt/(2*XMAX/3)) * SCALE
-    xpos_cos = XMAX/6 + XMAX/6*cos
-    ypos_cos = 2*YMAX/3 - xt
+    canvas.create_oval(xpos_sin-2*R, ypos_sin-2*R, xpos_sin+2*R, ypos_sin+2*R, outline=brightpink, fill=brightpink, tag='dots')
+    canvas.create_oval(xpos_cos-2*R, ypos_cos-2*R, xpos_cos+2*R, ypos_cos+2*R, outline=brightpink, fill=brightpink, tag='dots')
+    canvas.create_oval(xpos_circ-2*R, ypos_circ-2*R, xpos_circ+2*R, ypos_circ+2*R, outline=brightpink, fill=brightpink, tag='dots')
 
-    xpos_circ = XMAX/6 + XMAX/6*cos
-    ypos_circ = 5*YMAX/6 - YMAX/6*sin
-
-    canvas.create_oval(xpos_sin-2*R, ypos_sin-2*R, xpos_sin+2*R, ypos_sin+2*R, outline=orange, fill=orange, tag='dots')
-    canvas.create_oval(xpos_cos-2*R, ypos_cos-2*R, xpos_cos+2*R, ypos_cos+2*R, outline=orange, fill=orange, tag='dots')
-    canvas.create_oval(xpos_circ-2*R, ypos_circ-2*R, xpos_circ+2*R, ypos_circ+2*R, outline=orange, fill=orange, tag='dots')
-
-    canvas.create_line(xpos_circ, ypos_circ, xpos_sin, ypos_sin, width=WIDTH, fill=grey, tag='dots')
-    canvas.create_line(xpos_circ, ypos_circ, xpos_cos, ypos_cos, width=WIDTH, fill=grey, tag='dots')
-    canvas.create_line(XMAX/6, 5*YMAX/6, xpos_circ, ypos_circ, width=WIDTH, fill=grey, tag='dots')
+    canvas.create_line(xpos_circ, ypos_circ, xpos_sin, ypos_sin, width=WIDTH, fill=brightpurple, tag='dots')
+    canvas.create_line(xpos_circ, ypos_circ, xpos_cos, ypos_cos, width=WIDTH, fill=brightpurple, tag='dots')
+    canvas.create_line(XMAX/6, 5*YMAX/6, xpos_circ, ypos_circ, width=WIDTH, fill=brightpurple, tag='dots')
 
     window.update()
     time.sleep(DELAY)
